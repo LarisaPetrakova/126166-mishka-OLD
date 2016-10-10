9e2717c8e3315fea8eca0ed91d13e2e90d99bf94
larisapetrakova <larisa.petrakova@gmail.com>
2016-10-10 11:35:00 +0300

    начала делать js

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,23 @@
-var link = document.querySelector(".main-nav__toggle");
+var link = document.querySelector(".main-nav__open");
 var popup = document.querySelector(".hidden");
+var close = document.querySelector(".main-nav__close");
 
 link.addEventListener("click", function (event) {
     event.preventDefault();
-    popup.classList.toggle("hidden");
-    popup.classList.remove("modal-error");
+  popup.classList.add("hidden-show");
 });
 
+close.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.remove("hidden-show");
 
-window.addEventListener("keydown", function (event) {
-    if (event.keyCode === 27) {
-        popup.classList.contains("hidden");
-        popup.classList.remove("hidden");
-    }
-});
+      });
 
+window.addEventListener("keydown", function(event) {
+if (event.keyCode === 27) {
+          if (popup.classList.contains("hidden-show")) {
+            popup.classList.remove("hidden-show");
+
+          }
+        }
+      });
